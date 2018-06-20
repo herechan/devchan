868358b74d7f016ed4166d571e916b860ac8c87b
herechan <herechan@163.com>
2018-06-20 15:23:32 +0800

    website

---
+++ front-end/pages/admin.vue
@@ -0,0 +1,121 @@
+<template>
+    <div class="container">
+        <aside class="">
+            <div class="admin-logo">
+                <img src="~/assets/img/user.png" alt="">
+                <p class="admin-user">ChanAdmin</p>
+            </div>
+            <div class="devider"></div>
+            <div class="search-box">
+                <el-input v-model="search" class="search"></el-input>
+            </div>
+            <ul class="aside-list">
+                <li @click="triggerAside(item,index)" :class="{'aside-item-active':curIndex == index}" class="aside-item" v-for="(item, index) in asideList" :key="index">
+                    <i class="iconfont" v-html="item.icon"></i>{{item.name}}</li>
+            </ul>
+        </aside>
+        <header>
+            <p class="header-title">Writing Center</p>
+        </header>
+        <main>d</main>
+    </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      curIndex: 0,
+      search: "",
+      asideList: [
+        {
+          name: "Writing Center",
+          icon: "&#xe633;"
+        },
+        {
+          name: "Twitter",
+          icon: "&#xe749;"
+        },
+        {
+        name:"Draft",
+        icon:"&#xe64e;"
+        }
+      ]
+    };
+  },
+  methods: {
+    triggerAside(item, index) {
+      this.curIndex = index;
+    }
+  }
+};
+</script>
+<style lang="scss" scoped>
+aside {
+  height: 100vh;
+  width: 260px;
+  background-color: #fff;
+  box-shadow: 5px 0px 8px -4px rgba(0, 0, 0, 0.2);
+  position: absolute;
+  left: 0;
+  .admin-logo {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 20px;
+    img {
+      height: 56px;
+      width: 56px;
+      border-radius: 50%;
+    }
+    .admin-user {
+      font-size: 18px;
+      font-weight: bold;
+      margin-left: 10px;
+    }
+  }
+  .devider {
+    height: 1px;
+    background-color: $asideDevider;
+    margin: auto;
+    margin-bottom: 20px;
+  }
+  .search-box {
+    text-align: center;
+    padding: 0 20px 20px 20px;
+  }
+  .aside-list {
+    padding-left: 0;
+    .aside-item {
+      padding: 15px 20px;
+      transition: 0.1s ease;
+      user-select: none;
+      cursor: pointer;
+      display: flex;
+      align-items: center;
+      .iconfont {
+        margin-right: 10px;
+        font-size: 18px;
+      }
+    }
+    .aside-item-active {
+      color: #fff;
+      background-color: $mainColor;
+    }
+  }
+}
+header {
+  height: 70px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding-left: 280px;
+  .header-title{
+      font-size: 20px;
+      font-weight: 400;
+  }
+}
+main{
+    padding-left: 280px;
+}
+</style>
+
---
+++ front-end/pages/login.vue
@@ -0,0 +1,91 @@
+<template>
+    <div class="login-wrap">
+        <el-row>
+            <el-col :sm="12" class="login-side hidden-xs-only">
+                <div class="login-img"></div>
+            </el-col>
+            <el-col :sm="12" class="login-side">
+                <div class="login-box">
+                    <img src="~/assets/img/logo.png" alt="">
+                    <div class="login-inner">
+                        <div class="login-row">
+                            <p class="login-name">用户名</p>
+                            <el-input v-model="username"></el-input>
+                        </div>
+                        <div class="login-row">
+                            <p class="login-name">密码</p>
+                            <el-input v-model="password"></el-input>
+                        </div>
+                        <div class="login-row" style="text-align:right;margin-top:30px;">
+                            <el-button type="primary">登录</el-button>
+                        </div>
+                    </div>
+                    <p class="login-back">&#60; 返回首页</p>
+                </div>
+            </el-col>
+        </el-row>
+    </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      username: "",
+      password: ""
+    };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.login-wrap {
+  .login-side {
+    height: 100vh;
+    .login-img {
+      height: 100%;
+      width: 100%;
+      background: url("~/assets/img/login-img.jpg") no-repeat;
+      background-size: cover;
+      background-position: center;
+    }
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    .login-box {
+      width: 300px;
+      img {
+        height: 52px;
+        width: 52px;
+        border-radius: 50%;
+        margin: auto;
+        display: block;
+        margin-bottom: 20px;
+      }
+      .login-inner {
+        height: 270px;
+        background-color: $cardBg;
+        border-radius: 4px;
+        padding: 20px;
+        @include cardBorder;
+        .login-row {
+          .login-name {
+            margin-bottom: 5px;
+          }
+          margin-bottom: 20px;
+        }
+      }
+      .login-back{
+            font-size: 12px;
+            cursor: pointer;
+            color: $asideColor;
+            margin-top: 20px;
+            &:hover{
+                text-decoration: underline;
+            }
+            padding-left: 20px;
+        }
+    }
+  }
+}
+</style>
+
