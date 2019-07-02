821d19fba71697d9384757e847f32d7bdb571b99
herechan <herechan@163.com>
2019-07-02 22:25:29 +0800

    '接口改造'

--- server/app.js
+++ server/app.js
@@ -35,16 +35,30 @@ app.use(koaBody({
 
 app.use(router.routes());
 
-nuxtConfig.dev = process.env.NODE_ENV === "production"
+
+// nuxtConfig.dev = process.env.NODE_ENV === "production" 
+// 生产,环境,开发都为同一个打包模式，之前没有使用koa中间件渲染nuxt,所以这里暂时将三种模式用同一种方式打包
+if (process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'test' ||  process.env.NODE_ENV === 'test') {
+    nuxtConfig.dev = true
+} else {
+    nuxtConfig.dev = false
+}
 nuxtConfig.srcDir = path.resolve(__dirname, "../client");
 const nuxt = new Nuxt(nuxtConfig)
 
+
+app.use(nuxt.render)
 //当前为开发模式
 if (!nuxtConfig.dev) {
     const builder = new Builder(nuxt)
-    builder.build();
+    builder.build().then(listen());
+}else {
+    listen()
 }
-app.use(nuxt.render)
-app.listen(8009,"127.0.0.1",() => {
-    console.log("server start at http://127.0.0.1:8009")
-});
+
+
+function listen (){
+    app.listen(8009,"127.0.0.1",() => {
+        console.log("server start at http://127.0.0.1:8009")
+    });
+}
